30cf77f2c4b2cbb2d3f54e00a3ddcaad0af1f275
J0nathan12 <jgran12@ymail.com>
2017-06-08 13:02:47 -0400

    Create actions.py

--- clusterdock/topologies/cdh/actions.py
+++ clusterdock/topologies/cdh/actions.py
@@ -40,13 +40,13 @@ logger.setLevel(logging.INFO)
 DEFAULT_CLOUDERA_NAMESPACE = Constants.DEFAULT.cloudera_namespace # pylint: disable=no-member
 
 def start(args):
-    primary_node_image = "{0}/{1}/clusterdock:{2}_{3}_primary-node".format(
-        args.registry_url, args.namespace or DEFAULT_CLOUDERA_NAMESPACE,
+    primary_node_image = "{0}/cloudera/clusterdock:{2}_{3}_primary-node".format(
+        args.registry_url, args.namespace,
         args.cdh_string, args.cm_string
     )
 
-    secondary_node_image = "{0}/{1}/clusterdock:{2}_{3}_secondary-node".format(
-        args.registry_url, args.namespace or DEFAULT_CLOUDERA_NAMESPACE,
+    secondary_node_image = "{0}/cloudera/clusterdock:{2}_{3}_secondary-node".format(
+        args.registry_url, args.namespace,
         args.cdh_string, args.cm_string
     )
 
